3c82175933c9a693bf8d4654b5f27c34a3a26e09
Stefano Pirrello <spirrello@liaison.com>
2019-05-08 22:23:49 -0400

    using flags for variables

--- url-test.go
+++ url-test.go
@@ -13,15 +13,15 @@ import (
 	"time"
 )
 
-func httpTest(url *string, ch chan<- string, iteration int, outputOpt *string) {
-	//function for testing URLs
-	//url for target
-	//ch for channel
-	//iteration used for printing the test iteration
-	//output used for whether not to print http response
+//httpTest sends get requests to an endpoint.
+func httpTest(url *string, ch chan<- string, iteration int, httpBody *string, insecure *string) {
 
-	//Disable security check
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	//if insecure flag is true skip ssl verification
+	insecureStr := *insecure
+	if strings.Compare(insecureStr, "true") == 0 {
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+
 	//Clock the start and finish of each request
 	start := time.Now()
 	resp, err := http.Get(*url)
@@ -35,8 +35,8 @@ func httpTest(url *string, ch chan<- string, iteration int, outputOpt *string) {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		bodyString := string(bodyBytes)
 
-		outputStr := *outputOpt
-		if strings.Compare(outputStr, "true") == 0 {
+		httpBodyStr := *httpBody
+		if strings.Compare(httpBodyStr, "true") == 0 {
 			ch <- fmt.Sprintf("test: %d, time spent: %.2f seconds, result: %s, http output: %s", iteration, secs, resp.Status, bodyString)
 		} else {
 			ch <- fmt.Sprintf("test: %d, time spent: %.2f seconds, result: %s", iteration, secs, resp.Status)
@@ -46,31 +46,42 @@ func httpTest(url *string, ch chan<- string, iteration int, outputOpt *string) {
 
 }
 
+//THIS NEEDS WORK.  THE FOLLOWING IS FAILING.
+//inputValidation checks values added.
+// func inputValidation(url *string, request int, httpBody string) {
+
+// 	urlStr := *url
+
+// 	switch urlStr {
+// 	case strings.Contains(urlStr, "https://"):
+// 	case strings.Contains(urlStr, "http://"):
+// 	default:
+// 	}
+
+// }
+
 func main() {
 
 	// URL and number of parallel requests to make.
 	url := flag.String("url", "https://google.com", "URL and google.com by default")
 	requests := flag.Int("requests", 1, "number of concurrent requests")
-	output := flag.String("output", "false", "flag for printing output or not")
+	httpBody := flag.String("output", "false", "flag for printing http body or not")
+	insecure := flag.String("insecure", "true", "flag for when to ignore SSL errors")
 
 	flag.Parse()
 
 	fmt.Println("Testing with:", *url)
 
-	urlOpt := url
-	requestOpt := requests
-	outputOpt := output
-
 	//channel
 	ch := make(chan string)
 
 	//Send the requests
-	for iteration := 0; iteration < *requestOpt; iteration++ {
-		go httpTest(urlOpt, ch, iteration, outputOpt)
+	for iteration := 0; iteration < *requests; iteration++ {
+		go httpTest(url, ch, iteration, httpBody, insecure)
 
 	}
 	// Loop through the results
-	for i := 0; i < *requestOpt; i++ {
+	for i := 0; i < *requests; i++ {
 		log.Println(<-ch)
 	}
 
