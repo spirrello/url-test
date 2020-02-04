69ed432b1b8c07536bc07f851952330f53d95afc
Stefano Pirrello <spirrello.com>
2020-02-04 15:17:18 -0500

    testing env vars

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -e
+#set -e
 
 echo "#################################################"
 
 echo GOPATH: $GOPATH
 echo PWD: $PWD
 echo PROJECT_ROOT: $PROJECT_ROOT
+echo PROJECT_ID: $PROJECT_ID
 
 ls -ltr
 
